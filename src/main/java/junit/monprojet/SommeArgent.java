f56428f9fb5870e3ceff464da0df367c5cfd0c40
Zied <ziedmaghraoui58@gmail.com>
2024-11-18 09:58:15 +0100

    added equals override

---
+++ src/main/java/junit/monprojet/SommeArgent.java
@@ -0,0 +1,31 @@
+package junit.monprojet;
+
+public class SommeArgent {
+    private int quantite;
+    private String unite;
+
+    public SommeArgent(int amount, String currency) {
+        quantite = amount;
+        unite = currency;
+    }
+
+    public int getQuantite() {
+        return quantite;
+    }
+
+    public String getUnite() {
+        return unite;
+    }
+
+    public SommeArgent add(SommeArgent m) {
+        return new SommeArgent(getQuantite() + m.getQuantite(), getUnite());
+    }
+
+    public boolean equals(Object anObject){
+        if (!(anObject instanceof SommeArgent)){
+            return false;
+        }
+        SommeArgent convObject = (SommeArgent) anObject;
+        return this.quantite == convObject.getQuantite() && this.unite.equals(convObject.getUnite());
+    }
+}
